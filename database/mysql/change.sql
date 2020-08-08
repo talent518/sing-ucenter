af9ac9388896e5914a6d30664fc555a41e1ef812
阿宝 <talent518@yeah.net>
2020-08-08 18:42:58 +0800

    导出SQL

--- database/mysql/change.sql
+++ database/mysql/change.sql
@@ -42,7 +42,7 @@ CREATE TABLE `user_integral` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户星星数'
 /*!50100 PARTITION BY HASH (user_id)
 PARTITIONS 128 */;
 
@@ -53,14 +53,15 @@ DROP TABLE IF EXISTS `user_integral_log`;
 CREATE TABLE `user_integral_log` (
   `user_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '用户ID',
   `periods_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '期数ID',
+  `course_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '课程ID',
   `dest_type` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '目标类型(1教材,2环节,3学习报告,4调查问卷,5生成证书,6分享证书,7礼品兑换)',
   `dest_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '目标ID',
   `flag` smallint(4) unsigned NOT NULL DEFAULT '0' COMMENT '标示',
   `stars` int(10) NOT NULL DEFAULT '0' COMMENT '星星数',
   `remark` varchar(100) NOT NULL DEFAULT '' COMMENT '备注',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  PRIMARY KEY (`user_id`,`periods_id`,`dest_type`,`dest_id`,`flag`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+  PRIMARY KEY (`user_id`,`periods_id`,`course_id`,`dest_type`,`dest_id`,`flag`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户星星明细'
 /*!50100 PARTITION BY HASH (user_id)
 PARTITIONS 128 */;
 
@@ -74,7 +75,22 @@ CREATE TABLE `user_watch_time` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户看课总时长'
+/*!50100 PARTITION BY HASH (user_id)
+PARTITIONS 128 */;
+
+/*Table structure for table `user_watch_time_date` */
+
+DROP TABLE IF EXISTS `user_watch_time_date`;
+
+CREATE TABLE `user_watch_time_date` (
+  `user_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '用户ID',
+  `date` date NOT NULL DEFAULT '0000-00-00' COMMENT '日期',
+  `play_time` int(10) NOT NULL DEFAULT '0' COMMENT '播放时长',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`user_id`,`date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户每天看课时长'
 /*!50100 PARTITION BY HASH (user_id)
 PARTITIONS 128 */;
 
@@ -85,18 +101,17 @@ DROP TABLE IF EXISTS `user_watch_time_element`;
 CREATE TABLE `user_watch_time_element` (
   `user_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '用户ID',
   `periods_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '期数ID',
-  `class_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '班级ID',
+  `course_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '课程ID',
   `textbook_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '教材ID',
   `segment_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '环节ID',
   `element_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '素材ID',
   `duration` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '素材时长',
   `play_time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '播放时间',
-  `is_complete` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否已完成',
   `is_playable` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否可播放',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
-  PRIMARY KEY (`user_id`,`periods_id`,`class_id`,`textbook_id`,`segment_id`,`element_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+  PRIMARY KEY (`user_id`,`periods_id`,`course_id`,`textbook_id`,`segment_id`,`element_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户看课时长明细'
 /*!50100 PARTITION BY HASH (user_id)
 PARTITIONS 128 */;
 
