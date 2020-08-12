58704f302da5cf1704bb7630e6fcfd92fa836b4a
阿宝 <talent518@yeah.net>
2020-08-12 11:48:56 +0800

    事务处理。

--- commands/controllers/WatchController.php
+++ commands/controllers/WatchController.php
@@ -1,15 +1,17 @@
 <?php
 namespace app\commands\controllers;
 
-use yii\console\Controller;
 use app\models\watch\UserWatchTimeElement;
 use app\services\watch\CourseService;
+use yii\console\Controller;
+use yii\db\Transaction;
 
 class WatchController extends Controller {
 	public function actionRefresh() {
+		$transaction = UserWatchTimeElement::getDb()->beginTransaction(Transaction::SERIALIZABLE);
 		$list = UserWatchTimeElement::find()->all();
-		foreach($list as $model) { /* @var $model UserWatchTimeElement */
-			echo 'Element(', implode(', ', array_map(function($key) use($model) {
+		foreach($list as $i=>$model) { /* @var $model UserWatchTimeElement */
+			echo $i+1, ' - Element(', implode(', ', array_map(function($key) use($model) {
 					return $key . ': ' . $model->$key;
 				}, ['user_id', 'periods_id', 'course_id', 'textbook_id', 'segment_id', 'element_id'])), ') ... ';
 			
@@ -25,5 +27,6 @@ class WatchController extends Controller {
 			
 			echo $ret ? 'Success' : 'Failure', PHP_EOL;
 		}
+		$transaction->commit();
 	}
 }
