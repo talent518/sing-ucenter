ca3c8a376568cd397f96be467445d665ff82e151
阿宝 <talent518@yeah.net>
2020-08-18 10:02:19 +0800

    watch默认控制器。

---
+++ modules/watch/controllers/DefaultController.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\modules\watch\controllers;
+
+use yii\web\Controller;
+
+class DefaultController extends Controller {
+
+	public function actionIndex() {
+		exit(__METHOD__);
+	}
+
+}
